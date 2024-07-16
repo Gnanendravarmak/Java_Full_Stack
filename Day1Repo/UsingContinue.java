dcf2ffeacf6b37e2a86eea99b6cfa21d6deccb9e
Gnanendravarmak <112083635+Gnanendravarmak@users.noreply.github.com>
2024-07-16 08:46:02 +0530

    Update UsingContinue.java

--- Day1Repo/UsingContinue.java
+++ Day1Repo/UsingContinue.java
@@ -36,6 +36,39 @@ public class UsingContinue {
         }
         
         System.out.println(max);
+        
+        
+        
+        int num1 = 5;
+        int num2 = 10;
+        int num3 = 7;
+        int num4 = 3;
+        int num5 = 9;
+        
+ 
+        int num6 = (num1 > num2) ? ((num1 > num3) ? ((num1 > num4) ? ((num1 > num5) ? num1 : num5) : ((num4 > num5) ? num4 : num5)) : ((num3 > num4) ? ((num3 > num5) ? num3 : num5) : ((num4 > num5) ? num4 : num5)))
+                                : ((num2 > num3) ? ((num2 > num4) ? ((num2 > num5) ? num2 : num5) : ((num4 > num5) ? num4 : num5)) : ((num3 > num4) ? ((num3 > num5) ? num3 : num5) : ((num4 > num5) ? num4 : num5)));
+        
+        
+        System.out.println(num6);
+        
+        
+        
+        int maxVal;
+        if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5) {
+        	maxVal = num1;
+        } else if (num2 > num3 && num2 > num4 && num2 > num5) {
+        	maxVal = num2;
+        } else if (num3 > num4 && num3 > num5) {
+        	maxVal = num3;
+        } else if (num4 > num5) {
+        	maxVal = num4;
+        } else {
+        	maxVal = num5;
+        }
+        
+      
+        System.out.println(maxVal);
 
 	}
 
