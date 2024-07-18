bf163487c3cae6e47b8a9cd3f66cb83cab6dac0f
Gnanendravarmak <112083635+Gnanendravarmak@users.noreply.github.com>
2024-07-18 20:55:05 +0530

    Delete Day2Repo directory

--- Day2Repo/ClassB.java
+++
@@ -1,9 +0,0 @@
-package com.grayMatter;
-
-public class ClassB extends ClassA {
-	
-	public void printDeatails() {
-		System.out.println();
-	}
-
-}
--- Day2Repo/DemoOnOuter.java
+++
@@ -1,9 +0,0 @@
-package com.grayMatter;
-
-public class DemoOnOuter {
-	
-	public static void main(String[] args) {
-		OuterClass.Inner o = new OuterClass.Inner();
-	}
-
-}
